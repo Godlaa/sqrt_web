d1f6c96b2259ed2681cf605d4b993ba8e723802d
Эстокадо Фамилио <mimimilka55@gmail.com>
2024-05-17 00:03:01 +0500

    vse

--- src/App.js
+++ src/App.js
@@ -45,17 +45,21 @@ const App = () => {
 
   const [ value, setValue ] = useState('');
   const [ accuracy, setAccuracy ] = useState(3);
+  const [ result, setResult ] = useState('');
   const { t } = useTranslation();
 
   const calcSquareRoot = () => {
     const squareRoot = new SquareRoot(value, accuracy);
     const calcAnswer = squareRoot.calc();
-    if (calcAnswer ? alert(t('result').toString() + ': ' + squareRoot.calc().toString()) : alert('Ошибка вычисления'));
+    if (calcAnswer) {
+      setResult(t('result').toString() + ': ' + squareRoot.calc());
+    }
+    else setResult('Ошибка вычисления');
   }
 
   return (
     <Container>
-      <Container className="ms-5 mt-4 d-flex justify-content-end align-items-center">
+      <Container className="mt-5 d-flex justify-content-center align-items-center">
         <strong className="mx-3">{t('language')}:</strong>
         <Button variant="dark" className="mx-3" onClick={() => i18n.changeLanguage('sp')}>Español</Button>
         <Button variant="dark" className="mx-3" onClick={() => i18n.changeLanguage('hd')}>हिन्दी</Button>
@@ -71,18 +75,18 @@ const App = () => {
             <Form>
 
               <Form.Group className="d-flex justify-content-center align-items-center" controlId="numberInput">
-                <Row>
+                <Row >
                   <Col>
-                    <Form.Label className='mx-3'>{t('enterNumber')}</Form.Label>
+                    <Form.Label  className='mx-3 d-flex justify-content-center align-items-center'>{t('enterNumber')}</Form.Label>
                     <Form.Control value={value} onChange={e => setValue(e.target.value)} type="text" placeholder={t('enterNumber')} />
-                    <Form.Label className='mx-3 mt-5'>{t('enterAccuracy')}</Form.Label>
-                    <Form.Control value={accuracy} onChange={e => setAccuracy(e.target.value)} type="text" placeholder="Введите точность" />
+                    <Form.Label className='mx-3 mt-5 d-flex justify-content-center align-items-center'>{t('enterAccuracy')}</Form.Label>
+                    <Form.Control value={accuracy} onChange={e => setAccuracy(e.target.value)} type="text" placeholder={t('enterAccuracy')} />
+                    <Form.Label className='mx-3 mt-5 d-flex justify-content-center align-items-center'>{t('result')}</Form.Label>
+                    <Form.Control style={{width: '300px'}} type="text" readOnly placeholder={result} />
                   </Col>
-
                 </Row>
-
-                <Button className="m-5" variant="dark" onClick={calcSquareRoot}>{t('calculate')}</Button>
-              </Form.Group>
+                <Button className="mt-4 ms-5" variant="dark" onClick={calcSquareRoot}>{t('calculate')}</Button>
+              </Form.Group> 
             </Form>
           </Col>
         </Row>
