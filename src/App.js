472c97e4b33cb82f78025bf738f71b84c32e418b
Эстокадо Фамилио <mimimilka55@gmail.com>
2024-05-16 15:38:10 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import SquareRoot from './SquareRoot';
 
-function App() {
+const App = () => {
+
+  const [ value, setValue ] = useState('');
+  const [ accuracy, setAccuracy ] = useState(3);
+
+  const calcSquareRoot = () => {
+    const squareRoot = new SquareRoot(value, accuracy);
+    const calcAnswer = squareRoot.calc();
+    if (calcAnswer ? alert(squareRoot.calc()) : alert('Ошибка вычисления'));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="d-flex justify-content-center align-items-center" style={{ height: '100vh' }}>
+      <Row>
+        <Col>
+          <Form>
+
+            <Form.Group className="d-flex justify-content-center align-items-center" controlId="numberInput">
+              <Row>
+                <Col>
+                  <Form.Label className='mx-3'>Введите число</Form.Label>
+                  <Form.Control value={value} onChange={e => setValue(e.target.value)} type="text" placeholder="Введите число" />
+                  <Form.Label className='mx-3 mt-5'> Введите точность</Form.Label>
+                  <Form.Control value={accuracy} onChange={e => setAccuracy(e.target.value)} type="text" placeholder="Введите точность" />
+                </Col>
+
+              </Row>
+
+              <Button className="m-5" variant="dark" onClick={calcSquareRoot}>Посчитать квадратный корень</Button>
+            </Form.Group>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 }
+
 
 export default App;
---
+++ src/SquareRoot.js
@@ -0,0 +1,48 @@
+import Big from 'big.js';
+import Complex from 'complex.js';
+
+class SquareRoot {
+
+        constructor(value, accuracy){
+            this.value = value;
+            this.accuracy = Number(accuracy);
+        }
+
+        returnWithAccuracy(value){
+            if(isNaN(value)) return null;
+            if(value instanceof Big) return value.toFixed(this.accuracy);
+            if(value instanceof Complex) {
+                const re = value.re.toFixed(this.accuracy);
+                const im = value.im.toFixed(this.accuracy);
+                const answer = re + ' + ' + im + 'i';
+                return answer;
+            }
+        }
+
+        calc(){
+            let value = this.value;
+            if (value === '') return null;
+            else if(value.includes('i')) {
+                try {
+                    let complexNumber = new Complex(value);
+                    return this.returnWithAccuracy(complexNumber.sqrt());
+                }
+                catch (error) {
+                    return null;
+                }
+            }
+            else {
+                try {
+                    value = eval(value);
+                    const big_num = new Big(value);
+                    if (big_num.lt(0)) return null;
+                    return this.returnWithAccuracy(big_num.sqrt());
+                }
+                catch (error){
+                    return null;
+                }
+            }
+        }
+}
+
+export default SquareRoot;
